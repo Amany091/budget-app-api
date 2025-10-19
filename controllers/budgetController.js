0e6ce9520a2ac7f74d13ed2d2b77e4d35482c1c4
amany091 <2manym7md90@gmail.com>
2025-10-19 14:07:47 +0200

    update in design pattern

--- controllers/budgetController.js
+++ controllers/budgetController.js
@@ -3,9 +3,18 @@ const AppError = require('../utils/appError');
 const asyncWrapper = require('../utils/asyncWrapper');
 const {paginate} = require('../utils/paginate');
 
+const countBudgetTotals = async () => {
+    const budgetData = await Budget.find();
+    const income = budgetData.filter(item => item.type === 'income').reduce((acc, curr) => acc + curr.amount, 0);
+    const expense = budgetData.filter(item => item.type === 'expense').reduce((acc, curr) => acc + curr.amount, 0);
+    const total = income - expense;
+    return { income, expense, total };
+}
+
 exports.addBudget = asyncWrapper(async (req, res, next) => {
   const budget = await Budget.create(req.body);
-   return res.status(201).json({ status: "success", data: budget });
+  const { income, expense, total } = await countBudgetTotals();
+   return res.status(201).json({ status: "success", data: budget, income, expense, total });
 });
 
 exports.getBudget = asyncWrapper(async (req,res,next)=>{
@@ -17,22 +26,22 @@ exports.getBudget = asyncWrapper(async (req,res,next)=>{
     if(sortby === 'date') filter.sortby = {date: -1};
     const options = {page, limit};
     const result = await paginate(Budget, filter, options, {date: -1, amount: -1});
-    const income = result.data.filter(item => item.type === 'income').reduce((acc, curr) => acc + curr.amount, 0);
-    const expense = result.data.filter(item => item.type === 'expense').reduce((acc, curr) => acc + curr.amount, 0);
-    const total = income - expense;
+    const { income, expense, total } = await countBudgetTotals();
     return res.status(200).json({status: "success", data: result.data, pagination: result.pagination, income, expense, total});
 });
 
 exports.deleteBudget = asyncWrapper(async (req,res,next)=>{
     const {id} = req.params;
     const budget = await Budget.findByIdAndDelete(id);
+    const { income, expense, total } = await countBudgetTotals();
     if(!budget) return next(new AppError('No budget found with that ID', 404));
-    return res.status(200).json({message: "deleted successfully"})
+    return res.status(200).json({message: "deleted successfully", income, expense, total});
 });
 
 exports.updateBudget = asyncWrapper(async (req,res,next)=>{
     const {id} = req.params;
     const budget = await Budget.findByIdAndUpdate(id, req.body, {new: true, runValidators: true});
     if(!budget) return next(new AppError('No budget found with that ID', 404));
-    return res.status(200).json({status: "success", data: budget})
+    const { income, expense, total } = await countBudgetTotals();
+    return res.status(200).json({status: "success", data: budget, income, expense, total});
 })
